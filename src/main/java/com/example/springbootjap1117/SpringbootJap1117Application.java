5b6c1aac512d47816a2c0c31447a55379b74ff6d
Universenn <dnwndhks98@gmail.com>
2022-11-17 13:40:28 +0900

    first commit

---
+++ src/main/java/com/example/springbootjap1117/SpringbootJap1117Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootjap1117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJap1117Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJap1117Application.class, args);
+    }
+
+}
